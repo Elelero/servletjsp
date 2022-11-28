9003633e593350ae0c3cc49bb4c4f6770125cc0a
Elelero <hansumman618@gmail.com>
2022-11-28 16:30:19 +0900

    2022-11-28 수업 (서블릿)

--- src/servlet/Exam01Controller.java
+++ src/servlet/Exam01Controller.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,28 +14,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import dto.Board;
 
-@WebServlet("/Exam01Controller")
+@WebServlet(value="/Exam01Controller", loadOnStartup=1)
 public class Exam01Controller extends HttpServlet {
 	
 	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Exam01Controller.init() 실행");
+	}
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Board> boards = new ArrayList<>();
-		
-		for(int i=1; i<=10; i++){
-			Board board = new Board();
-			board.setBno(i);
-			board.setBtitle("제목" + i);
-			board.setBcontent("내용" + i);
-			board.setBwriter("홍길동");
-			board.setBdate(new Date());
-			boards.add(board);
-		}
-		
-		//처리 결과를 JSP에서 사용할 수 있도록 설정
-		request.setAttribute("list", boards);
+		System.out.println("service() 실행");
 		
 		//JSP로 이동
-		request.getRequestDispatcher("views/exam01_view.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/exam01/content.jsp").forward(request, response);
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("Exam01Controller.destroy() 실행");
 	}
 
 }
